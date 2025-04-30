58fedf8b2905509ee98acf17e7d591596a1f7aaa
saisripooja <psspooja1722@gmail.com>
2025-04-30 11:22:48 +0530

    Delete genus.cmd

--- genus.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 21.14-s082_1, built Jun 23 2022 14:32:08
-
-# Date: Thu Mar 20 10:36:46 2025
-# Host: user3.nielitchennai.edu.in (x86_64 w/Linux 4.18.0-425.3.1.el8.x86_64) (16cores*24cpus*1physical cpu*13th Gen Intel(R) Core(TM) i7-13700 30720KB)
-# OS:   Red Hat Enterprise Linux release 8.7 (Ootpa)
-
-source ram.tcl
